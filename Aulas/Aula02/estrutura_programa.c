978d28815bc916911c34c1476fe68fed0dfe8288
Mylena Alves de Souza <mylena.alves1322@gmail.com>
2025-08-20 10:49:45 -0300

    codigos da Aula02 e pratica01

---
+++ Aulas/Aula02/estrutura_programa.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+// 1.Entrada
+// Definir variaveis e constantes
+// Atribuição e leitura
+
+// 2. Processamento 
+// Operações + - * / ...
+
+// 3. Saída
+// Exibir o resultado
+
+return 0;
+
+}
---
+++ Praticas/pratica01/ola_mundo.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Bem-vindo à disciplina de APC!\n");
+    return 0;
+}
